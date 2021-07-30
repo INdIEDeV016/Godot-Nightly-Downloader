30d6af023ff98ba7abef46f53f638e8cf22289f8
INdIEDeV016 <80198819+INdIEDeV016@users.noreply.github.com>
2021-07-30 10:57:33 +0530

    Update About.gd

--- About.gd
+++ About.gd
@@ -1,11 +1,5 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$NextButton.scene = load("res://Welcome.tscn")
